6c09c7127a68ffc3260295642f26033e88acc093
John <example@gmail.com>
2020-05-17 13:27:19 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+//alert('Hello');
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = prompt('Вам есть 18?', '18');
+// console.log(answer+5);
+
+// const answers = [];
+
+// // answers[0] = prompt('Как ваше имя?','');
+// // answers[1] = prompt('Как ваша фамилия?','');
+// // answers[2] = prompt('Сколькл вам лет?','');
+
+// console.log(typeof(123));
+
+const category = 'toys';
+
+console.log(`https://someurl.com/${category}/5`);
+
+const user = 'Ivan';
+
+alert(`Привет, ${user}`)
