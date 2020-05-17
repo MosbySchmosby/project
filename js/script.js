faf782cd09d3850e3aa14296f78a9df4ffa4a2a0
John <example@gmail.com>
2020-05-17 16:46:35 +0200

    add git ignore

--- js/script.js
+++ js/script.js
@@ -16,10 +16,31 @@
 
 // console.log(typeof(123));
 
-const category = 'toys';
+// const category = 'toys';
 
-console.log(`https://someurl.com/${category}/5`);
+// console.log(`https://someurl.com/${category}/5`);
 
-const user = 'Ivan';
+// const user = 'Ivan';
 
-alert(`Привет, ${user}`)
+// alert(`Привет, ${user}`)
+
+let incr = 10,
+    decr = 10;
+// ++incr;
+// --decr;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+console.log(2*4 === 8);
+
+&&
+||
+
+const isChecked = true,
+    isClose = false;
+
+console.log(isChecked || isClose);
+
